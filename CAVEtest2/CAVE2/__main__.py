1cb5c4797cbf4dc67e55771bfe5c790bcf2b1cde
Chigwi <145591851+Chigwi@users.noreply.github.com>
2026-04-19 13:42:43 -0500

    function for roms installation

--- CAVEtest2/CAVE2/__main__.py
+++ CAVEtest2/CAVE2/__main__.py
@@ -1,14 +1,41 @@
 
+import sys
 import os
+import shutil
 
-def main():
-    print("Hello World")
+def resource_path(relative):
+    base = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base, relative)
 
+def install_roms():
+
+    #checks if retroarch is installed
     if (1 == 1):
         os.system('sudo apt install retroarch')
     elif(2 == 2):
         os.system('sudo dnf install retroarch')
     else:
         os.system('sudo pacman -S retroarch')
-    os.system('mkdir ~/.config/retroarch/ROMS')
-    os.system('cp -r ROMS ~/.config/retroarch/ROMS')
+
+    #path where the ROMs are being stored
+    source = resource_path('resources/ROMS')
+
+    #path of where the ROMS should be saved for retroarch use
+    dest = os.path.expanduser('~/.config/retroarch/ROMS')
+
+    print(f"Installing ROMS into {dest}")
+
+    #clean install protocol
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+
+
+    shutil.copytree(source, dest)
+
+    os.system(f"chmod -R 755 '{dest}'")
+
+    print("Installation complete")
+
+if __name__ == "__main__":
+    install_roms()
+
