f09633a3124122690fbf6b62f6027bb55d883ffa
SysZ2211 <anothersamwithz@gmail.com>
2026-04-19 13:26:39 -0500

    instalacion de retroarch dependiendo de la distro

--- CAVEtest2/CAVE2/__main__.py
+++ CAVEtest2/CAVE2/__main__.py
@@ -4,5 +4,11 @@ import os
 def main():
     print("Hello World")
 
-    os.system('mkdir ROMS')
-    os.system('cp -r ')
+    if (1 == 1):
+        os.system('sudo apt install retroarch')
+    elif(2 == 2):
+        os.system('sudo dnf install retroarch')
+    else:
+        os.system('sudo pacman -S retroarch')
+    os.system('mkdir ~/.config/retroarch/ROMS')
+    os.system('cp -r ROMS ~/.config/retroarch/ROMS')
