fa8dfc9b5fc4c22041451815c1432e55a22ca5cc
Chigwi <145591851+Chigwi@users.noreply.github.com>
2026-04-19 19:13:00 -0500

    brought base main from cave 2

--- CAVEtest-3/CAVE3/__main__.py
+++ CAVEtest-3/CAVE3/__main__.py
@@ -1,2 +1,73 @@
+import sys
+import os
+import shutil
+import subprocess
 
-print('spp')
+
+# creates the resource relative path to the ROMS packaged inside
+def resource_path(relative):
+    base = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base, relative)
+
+
+# checks the current linux distro
+def check_disto():
+    # rcieves the output containing the current distro
+    ret = subprocess.run(['cat', '/etc/os-release'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    # lower case xd
+    output = ret.stdout.lower()
+    # checks the output to see which distro is current and returns
+    if ('debian' in output or 'ubuntu' in output):
+        return 'apt'
+    elif ('fedora' in output):
+        return 'dnf'
+    else:
+        return 'pacman'
+
+
+# install retroarch
+def install_retroarch(distro):
+    # checks if retroarch is already installed
+    if (shutil.which('retroarch') is not None):
+        print('spp: Sub Proseso de Paso')
+        return
+    # installs retroarch acording to the current distro
+    elif (distro == 'apt'):
+        os.system('sudo apt install retroarch')
+    elif (distro == 'dnf'):
+        os.system('sudo dnf install retroarch')
+    elif (distro == 'pacman'):
+        os.system('sudo pacman -S retroarch')
+
+
+# main function installs ROMS from package
+def install_roms():
+    # checks distro
+    distro = check_disto()
+
+    # installs retroarch
+    install_retroarch(distro)
+
+    # path where the ROMs are being stored in package
+    source = resource_path('resources/ROMS')
+
+    # path of where the ROMS should be saved for retroarch use
+    dest = os.path.expanduser('~/.config/retroarch/ROMS')
+
+    print(f"Installing ROMS into {dest}")
+
+    # clean install protocol
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+
+    # moves the ROMS to target dir
+    shutil.copytree(source, dest)
+
+    # modifies the permissions of the ROM files
+    os.system(f"chmod -R 755 '{dest}'")
+
+    print("Installation complete")
+
+
+if __name__ == "__main__":
+    install_roms()
