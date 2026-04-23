f205398b333b22d15ea1e56a6a85d280f71ef332
Chigwi <145591851+Chigwi@users.noreply.github.com>
2026-04-23 18:14:46 -0500

    clase core para la instalación de los cores de emulación

--- CAVEtest-3/CAVE3/installer/Rom.py
+++ CAVEtest-3/CAVE3/installer/Rom.py
@@ -36,30 +36,30 @@ class ROMInstaller:
                 os.system('sudo pacman -S retroarch')
 
 
-# main function installs ROMS from package
-def _install_roms(self):
+    # main function installs ROMS from package
+    def _install_roms(self):
 
-    # path where the ROMs are being stored in package
-    source = self._resource_path('resources/ROMS')
+        # path where the ROMs are being stored in package
+        source = self._resource_path('resources/ROMS')
 
-    # path of where the ROMS should be saved for retroarch use
-    print(f"Installing ROMS into {self.dest}")
+        # path of where the ROMS should be saved for retroarch use
+        print(f"Installing ROMS into {self.dest}")
 
-    # clean install protocol
-    if os.path.exists(self.dest):
-        shutil.rmtree(self.dest)
+        # clean install protocol
+        if os.path.exists(self.dest):
+            shutil.rmtree(self.dest)
 
-    # moves the ROMS to target dir
-    shutil.copytree(source,self.dest)
+        # moves the ROMS to target dir
+        shutil.copytree(source,self.dest)
 
-    # modifies the permissions of the ROM files
-    os.system(f"chmod -R 755 '{self.dest}'")
+        # modifies the permissions of the ROM files
+        os.system(f"chmod -R 755 '{self.dest}'")
 
-    print("Installation complete")
+        print("Installation complete")
 
-def resource_path(relative):
-    base = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
-    return os.path.join(base, relative)
+    def resource_path(relative):
+        base = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+        return os.path.join(base, relative)
 
 
 
--- CAVEtest-3/CAVE3/installer/core.py
+++ CAVEtest-3/CAVE3/installer/core.py
@@ -12,3 +12,43 @@ class CoreInstaller:
         # adds the path of where the ROMS are
         self.dest = os.path.expanduser('~/.config/retroarch/cores')
 
+# install retroarch, hopefully dead code as retroarch should already be installed
+    def _install_retroarch(self, package_manager):
+            # checks if retroarch is already installed
+            if (shutil.which('retroarch') is not None):
+                print('Retroarch executable found skipping installation')
+                return
+            # installs retroarch acording to the current distro
+            elif (package_manager == 'apt'):
+                os.system('sudo apt install retroarch')
+            elif (package_manager == 'dnf'):
+                os.system('sudo dnf install retroarch')
+            elif (package_manager == 'pacman'):
+                os.system('sudo pacman -S retroarch')
+
+
+    #main function, installs the needed cores onto the retroarch machine
+    def _install_cores(self):
+
+        #path where the cores are being stored in package
+        source = self._source_path('resources/cores')
+
+        #path of where the cores should be saved for retroarch use
+        print(f'Installing cores into {self.dest}')
+
+        #clean install protocol
+        if os.path.exists(self.dest):
+            shutil.rmtree(self.dest)
+
+        #moves the cores to the target dir
+        shutil.copytree(source, self.dest)
+
+        #modifies the permissions of the core files
+        os.system(f"chmod -R 755  '{self.dest}'")
+
+        print("Cores successfully installed")
+
+    def resource_path(relative):
+        base = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+        return os.path.join(base, relative)
+
