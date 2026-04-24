c2769a6cf199b07eb7c00da6982523da0ad4e9fb
Chigwi <145591851+Chigwi@users.noreply.github.com>
2026-04-23 22:33:17 -0500

    refractor the imports

--- CAVEtest-3/CAVE3/__main__.py
+++ CAVEtest-3/CAVE3/__main__.py
@@ -1,5 +1,10 @@
-from installer.installationManager import InstallationManager
+import sys
+import os
 
+# makes sure the CAVE3 package is always findable
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from CAVE3.installer.installationManager import InstallationManager
 #main function runs the installer
 if __name__ == "__main__":
     manager = InstallationManager()
